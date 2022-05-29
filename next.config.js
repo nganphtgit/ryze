44d78d38797a5c5a078cc2493f42cb3a0b44e793
nganpht <tuyetnganphamhoang@gmail.com>
2022-05-29 23:34:10 +0700

    deploy by github actions
    github actions
    
    node ver
    
    test
    
    after add key
    
    test ref
    
    add action.yaml
    
    remove stupid action.yml
    
    config assets prefix
    
    config static
    
    deploy to gh-pages
    
    revert content

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/nganphtgit/ryze/main/' : '',
   reactStrictMode: true,
-}
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const Banner = () => {
+  return (
+    <section>
+      <div className='container'>
+        <h1>Hellooooooo</h1>
+      </div>
+    </section>
+  );
+};
+
+export function getStaticProps() {
+  const files = fs.readdirSync(path.join(process.cwd(), 'public', 'posts'));
+
+  return {
+    props: {},
+  }
+}
+
+export default function Blog() {
+  return (
+    <>
+    <Banner/>
+    </>
+  );
+};
