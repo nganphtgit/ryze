0991ca0d5eb558524a479b26ef90972cee6f3d6c
nganphtgit <35426376+nganphtgit@users.noreply.github.com>
2022-06-18 17:27:20 +0000

    Deploying to gh-pages from @ nganphtgit/ryze@c87c05df5c0910ab9f8591f2d2e0b95b7f1a8636 🚀

---
+++ _next/static/AlU5Si7flspjYJc5PNf9G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/adf6895ec356eb64.css","static/chunks/pages/index-16eb7cef399127ad.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/blog":["static/chunks/pages/blog-e3d29e7651110fec.js"],sortedPages:["/","/_app","/_error","/blog"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ posts/intro.md
@@ -0,0 +1,15 @@
+---
+author : "Julie"
+categories : ["Life"]
+date : "2022-05-28T18:25:23.430Z"
+description : "Daily life"
+image : "img/posts/introduction.jpeg"
+images : ["img/posts/introduction.jpeg"]
+slug : "introduction"
+summary : "Daily life"
+tags : ["life"]
+title : "Introduction"
+draft : false
+---
+
+hello
