f40f5bf608e845ff7dcbb04e216ea1adb4222a04
nganphtgit <35426376+nganphtgit@users.noreply.github.com>
2022-06-26 16:20:02 +0000

    Deploying to gh-pages from @ nganphtgit/ryze@5f4983b8ee5ee0fd9d9fc747dc9662835544115f 🚀

---
+++ _next/static/6XyJNbN1_DTsAg1Y15rHP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9f17ded1465ab744.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/blog":["static/chunks/pages/blog-e3d29e7651110fec.js"],sortedPages:["/","/_app","/_error","/blog"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
