06071b3505ac4239aa3286203ff5cbc19abe85e3
nganpht <tuyetnganphamhoang@gmail.com>
2022-06-26 23:34:36 +0700

    remove statically

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/nganphtgit/ryze/main/' : '',
   reactStrictMode: true,
   images: {
     loader: 'akamai',
